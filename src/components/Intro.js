2b0abc58056de57f7439b6c51db90c2ebfd6d6ce
unknown <spenceaw@gmail.com>
2021-02-23 15:06:19 -0500

    changed intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
       <img src={zion} className='prof-pic' alt='alex in zion' />
       <h1 className='title'>Alexander Warfield Spencer</h1>
       <div className='inner'>
-        <h2 className='header'>Web Developer, ready to join the team.</h2>
+        <h2 className='header'>Software Developer, ready to join the team.</h2>
       </div>
     </div>
   );
